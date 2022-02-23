023c5a43dd334149ae8837c025bc9f4c74325165
Nick Russell <n1russell@student.bridgew.edu>
2022-02-23 13:00:38 -0500

    1:00PM 2/23/22

--- CheckpointProject2.py
+++ CheckpointProject2.py
@@ -2,3 +2,5 @@
 #This is the checkpoint portion of Project 2.
 #This program reads in data from the keyboard, and draws an image specified by user input
 #using the Zelle graphics library.
+
+import random
