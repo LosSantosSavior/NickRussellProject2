952f3b3ffde447231f299d885694f5e1010e3973
Nick Russell <n1russell@student.bridgew.edu>
2022-02-23 13:14:28 -0500

    1:14PM 2/23/22

--- CheckpointProject2.py
+++ CheckpointProject2.py
@@ -3,4 +3,4 @@
 #This program reads in data from the keyboard, and draws an image specified by user input
 #using the Zelle graphics library.
 
-import random
+from random import *
--- NickRussellProject2.py
+++ NickRussellProject2.py
@@ -1,4 +1,4 @@
 #Nick Russell
 #This is the final code portion of Project 2.
 
-import graphics
+from graphics import *
